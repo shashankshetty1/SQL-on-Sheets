145a10809eae9ed0d6dadca6d28c55b3471e1a59
shashankshetty1 <shettyshashank089@gmail.com>
2026-02-07 21:05:08 +0530

    Initial commit - SQL CSV practice

---
+++ query.py
@@ -0,0 +1,21 @@
+import duckdb
+import pandas as pd
+
+con = duckdb.connect()
+
+df = con.execute("""
+                 
+
+SELECT 
+    COUNT(*) AS discovery_count,
+    SUM(TOTAL_AMOUNT) AS discovery_total_amount
+FROM read_csv_auto('data.csv')
+WHERE STAGE_NAME = '3 Discovery';
+
+
+
+                 
+                 
+""").fetchdf()
+
+print(df.to_string(index=False))
